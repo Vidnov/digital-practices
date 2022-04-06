93cc6a94af172451e83ad14fc5b576b9395ff065
nvidnov <nikitavidnov@yandex.ru>
2022-04-06 22:40:28 +0300

    master - рефакторинг сервера отображения

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+
 const app = express();
-const port = 3003;
 
-app.use(express.static("dist"));
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index");
-});
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("App run port", port);
 });
